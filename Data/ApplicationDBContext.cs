ee6c041377ff4dfded340f4156321f669acee09b
cangjie <cangjie@hotmail.com>
2021-07-08 15:21:08 +0800

    new dir for list

---
+++ Data/ApplicationDBContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using SnowmeetApi.Models;
+
+namespace SnowmeetApi.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<MaintainLive>().HasKey(c => c.id);
+        }
+
+        public DbSet<MaintainLive> MaintainLives {get; set;}
+    }
+}
---
+++ Models/MaintainLive.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SnowmeetApi.Models
+{
+    [Table("maintain_in_shop_request")]
+    public class MaintainLive
+    {
+        [Key]
+        public int id { get;}
+        public string shop { get; set; }
+        public string open_id { get; set; }
+        public string equip_type { get; set; }
+        public string brand { get; set; }
+        public string scale { get; set; }
+        public int edge { get; set; }
+        public int candle { get; set; }
+        public int repair_more { get; set; }
+        public DateTime? pick_date { get; set; }
+        public int task_id { get; set; }
+        public int order_id { get; set; }
+        public string service_open_id { get; set; }
+        public string confirmed_equip_type { get; set; }
+        public string confirmed_brand { get; set; }
+        public string confirmed_serial { get; set; }
+        public string confirmed_scale { get; set; }
+        public string confirmed_year { get; set; }
+        public int confirmed_edge { get; set; }
+        public int confirmed_degree { get; set; }
+        public int confirmed_candle { get; set; }
+        public string confirmed_more { get; set; }
+        public string confirmed_memo { get; set; }
+        public DateTime confirmed_pick_date { get; set; }
+        public double confirmed_additional_fee { get; set; }
+        public string confirmed_cell { get; set; }
+        public string confirmed_name { get; set; }
+        public string confirmed_gender { get; set; }
+        public int confirmed_product_id { get; set; }
+        public string confirmed_images { get; set; }
+        public int batch_id { get; set; }
+        public int label_printed { get; set; }
+        public string? task_flow_num { get; set; }
+        public int finish { get; set; }
+        public DateTime create_date { get; set; }
+
+    }
+}
+
