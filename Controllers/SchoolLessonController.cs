89bec9e9ea3f223d25cd3c4f8174c0bf2cb5fe27
cangjie <cangjie@hotmail.com>
2021-07-15 16:56:03 +0800

    first publish

--- Controllers/SchoolLessonController.cs
+++ Controllers/SchoolLessonController.cs
@@ -10,7 +10,7 @@ using SnowmeetApi.Models;
 
 namespace SnowmeetApi.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class SchoolLessonController : ControllerBase
     {
