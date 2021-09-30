9d0b618351948807e092fe272853aca7b9b00a50
cangjie <cangjie@hotmail.com>
2021-09-30 14:16:43 +0800

    add rfid model

---
+++ Models/rfid/Command.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace SnowmeetApi.Models.rfid
+{
+    [Table("rfid_command")]
+    public class Command
+    {
+        public Command()
+        {
+        }
+        [Key]
+        public int id { get; set; }
+
+        public string device_id { get; set; }
+        public string command { get; set; }
+        public string data { get; set; }
+        public int deal { get; set; }
+
+    }
+}
