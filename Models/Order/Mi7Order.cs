6eeb9efcdfa39acdc9c2a8a1fec56b6c6c26abab
cangjie <cangjie@hotmail.com>
2022-07-05 18:53:47 +0800

    add new class

---
+++ Models/Order/Mi7Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace SnowmeetApi.Models.Order
+{
+    [Table("mi7_order")]
+    public class Mi7Order
+    {
+        [Key]
+        public int id { get; set; }
+        public int order_id { get; set; }
+        public string mi7_order_id { get; set; }
+        public double sale_price { get; set; }
+        public double real_charge { get; set; }
+
+    }
+}
+
---
+++ Models/Order/Order.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace SnowmeetApi.Models.Order
+{
+    [Table("order_online")]
+    public class Order
+    {
+        [Key]
+        public int id { get; set; }
+
+        public string type { get; set; }
+        public string open_id { get; set; }
+        public string cell_number { get; set; }
+        public string name { get; set; }
+        public string pay_method { get; set; }
+        public double order_price { get; set; }
+        public double order_real_pay_price { get; set; }
+        public int pay_state { get; set; }
+        public DateTime pay_time { get; set; }
+        public string code { get; set; }
+        public string syssn { get; set; }
+        public string memo { get; set; }
+        public string shop { get; set; }
+        public int? mchid { get; set; }
+        public double ticket_amount { get; set; }
+        public double score_rate { get; set; }
+        public int generate_score { get; set; }
+        public string ticket_code { get; set; }
+        public string out_trade_no { get; set; }
+    }
+}
+
