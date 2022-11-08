0ae9f730fb6830ab1836d25976c8f5994c52e5ac
cangjie <cangjie@hotmail.com>
2022-11-08 10:23:39 +0800

    new fields

--- Models/Experience.cs
+++ Models/Experience.cs
@@ -8,29 +8,49 @@ namespace SnowmeetApi.Models
     public class Experience
     {
         [Key]
+
+
+
+
+
         public int id { get; set; }
 
 
-
         public string shop { get; set; } = "";
-        public double guarantee_cash { get; set; } = 0;
-        public int guarantee_order_id { get; set; } = 0;
-        public string cell_number { get; set; } = "";
-        public string open_id { get; set; } = "";
-        public string return_memo { get; set; } = "";
-        public double refund_amount { get; set; } = 0;
-
-        public string ticket_code { get; set; } = "";
-
-        public string staff_open_id { get; set; } = "";
 
         public string asset_name { get; set; } = "";
         public string asset_scale { get; set; } = "";
         public string asset_photos { get; set; } = "";
 
+        public double guarantee_cash { get; set; } = 0;
+        public int guarantee_order_id { get; set; } = 0;
+
+        public string guarantee_credential_type { get; set; } = "";
+
+        public string guarantee_credential_no { get; set; } = "";
+
+        public string guarantee_credential_photos { get; set; } = "";
+
         public DateTime? start_time { get; set; } = DateTime.Now;
 
         public DateTime? end_time { get; set; } = DateTime.Now.AddHours(2);
+
+        public string return_memo { get; set; } = "";
+        public double refund_amount { get; set; } = 0;
+
+
+
+        public string cell_number { get; set; } = "";
+        public string open_id { get; set; } = "";
+        
+
+        public string ticket_code { get; set; } = "";
+
+        public string staff_open_id { get; set; } = "";
+
+        
+
+        
 
         public DateTime create_date { get; set; } = DateTime.Now;
 
