3b8bae1a4683e7251ae3880bde4501d3b0b2db4e
cangjie <cangjie@hotmail.com>
2022-11-14 16:36:39 +0800

    set guarntee cash

--- Controllers/Order/OrderPaymentController.cs
+++ Controllers/Order/OrderPaymentController.cs
@@ -385,8 +385,8 @@ namespace SnowmeetApi.Controllers.Order
             int mchId = 3;
             if (order.type == "押金")
             {
-                //mchId = 5;
-                mchId = 3;
+                mchId = 5;
+                //mchId = 3;
             }
             if (order.type != "雪票" && order.shop == "南山")
             {
