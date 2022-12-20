3b68573c7fab7947e42f4d604ffa85dcd071bfcd
cangjie <cangjie@hotmail.com>
2022-12-20 12:21:31 +0800

    push message

--- Controllers/MiniAppHelperController.cs
+++ Controllers/MiniAppHelperController.cs
@@ -50,106 +50,109 @@ namespace LuqinMiniAppBase.Controllers
         }
 
 
-        /*
-                [HttpPost]
-                public async Task<ActionResult<string>> PushMessage([FromQuery] string signature,
-                    [FromQuery] string timestamp, [FromQuery] string nonce)
+        
+        [HttpPost]
+        public async Task<ActionResult<string>> PushMessage([FromQuery] string signature,
+            [FromQuery] string timestamp, [FromQuery] string nonce)
+        {
+            string[] validStringArr = new string[] { _settings.token.Trim(), timestamp.Trim(), nonce.Trim() };
+            Array.Sort(validStringArr);
+            string validString = String.Join("", validStringArr);
+            SHA1 sha = SHA1.Create();
+            ASCIIEncoding enc = new ASCIIEncoding();
+            byte[] bArr = enc.GetBytes(validString);
+            bArr = sha.ComputeHash(bArr);
+            string validResult = "";
+            for (int i = 0; i < bArr.Length; i++)
+            {
+                validResult = validResult + bArr[i].ToString("x").PadLeft(2, '0');
+            }
+            if (validResult != signature)
+            {
+                return NoContent();
+            }
+            string body = "";
+            var stream = Request.Body;
+            if (stream != null)
+            {
+                using (var reader = new StreamReader(stream, Encoding.UTF8, true, 1024, true))
                 {
-                    string[] validStringArr = new string[] { _settings.token.Trim(), timestamp.Trim(), nonce.Trim() };
-                    Array.Sort(validStringArr);
-                    string validString = String.Join("", validStringArr);
-                    SHA1 sha = SHA1.Create();
-                    ASCIIEncoding enc = new ASCIIEncoding();
-                    byte[] bArr = enc.GetBytes(validString);
-                    bArr = sha.ComputeHash(bArr);
-                    string validResult = "";
-                    for (int i = 0; i < bArr.Length; i++)
+                    body = await reader.ReadToEndAsync();
+
+                    string path = $"{Environment.CurrentDirectory}";
+                    string dateStr = DateTime.Now.Year.ToString() + DateTime.Now.Month.ToString().PadLeft(2, '0')
+                        + DateTime.Now.Day.ToString().PadLeft(2, '0');
+                    if (path.StartsWith("/"))
                     {
-                        validResult = validResult + bArr[i].ToString("x").PadLeft(2, '0');
+                       path = path + "/";
                     }
-                    if (validResult != signature)
+                    else
                     {
-                        return NoContent();
+                        path = path + "\\";
                     }
-                    string body = "";
-                    var stream = Request.Body;
-                    if (stream != null)
+                    path = path + "wechat_post_" + dateStr + ".txt";
+                    using (StreamWriter fw = new StreamWriter(path, true))
                     {
-
-                        using (var reader = new StreamReader(stream, Encoding.UTF8, true, 1024, true))
-                        {
-                            body = await reader.ReadToEndAsync();
-
-                            string path = $"{Environment.CurrentDirectory}";
-                            string dateStr = DateTime.Now.Year.ToString() + DateTime.Now.Month.ToString().PadLeft(2, '0')
-                                + DateTime.Now.Day.ToString().PadLeft(2, '0');
-                            if (path.StartsWith("/"))
-                            {
-                                path = path + "/";
-                            }
-                            else
-                            {
-                                path = path + "\\";
-                            }
-                            path = path + "wechat_post_" + dateStr + ".txt";
-                            using (StreamWriter fw = new StreamWriter(path, true))
-                            {
-                                fw.WriteLine(body.Trim());
-                                fw.Close();
-                            }
-                        }
-
+                        fw.WriteLine(body.Trim());
+                        fw.Close();
                     }
-                    try
-                    {
-                        XmlDocument xmlD = new XmlDocument();
-                        xmlD.LoadXml(body);
-                        XmlNode root = xmlD.SelectSingleNode("//xml");
-
-                        string eventStr = "";
-                        string eventKey = "";
-                        string content = "";
-                        string msgId = "";
-                        string msgType = root.SelectSingleNode("MsgType").InnerText.Trim();
-
-                        if (msgType.Trim().Equals("event"))
-                        {
-                            eventStr = root.SelectSingleNode("Event").InnerText.Trim();
-                            eventKey = root.SelectSingleNode("EventKey").InnerText.Trim();
-                        }
-                        else
-                        {
-                            content = root.SelectSingleNode("Content").InnerText.Trim();
-                            msgId = root.SelectSingleNode("MsgId").InnerText.Trim();
-                            msgType = root.SelectSingleNode("MsgType").InnerText.Trim();
-                        }
-
-                        OAReceive msg = new OAReceive()
-                        {
-                            id = 0,
-                            ToUserName = root.SelectSingleNode("ToUserName").InnerText.Trim(),
-                            FromUserName = root.SelectSingleNode("FromUserName").InnerText.Trim(),
-                            CreateTime = root.SelectSingleNode("CreateTime").InnerText.Trim(),
-                            MsgType = msgType,
-                            Event = eventStr,
-                            EventKey = eventKey,
-                            MsgId = msgId,
-                            Content = content
-
-                        };
-                        await _db.oAReceive.AddAsync(msg);
-                        await _db.SaveChangesAsync();
-                        OfficailAccountReply reply = new OfficailAccountReply(_db, _config, msg);
-                        return reply.Reply().Trim();
-                    }
-                    catch
-                    {
-
-                    }
-                    return "success";
                 }
 
+            }
+            try
+            {
+                XmlDocument xmlD = new XmlDocument();
+                xmlD.LoadXml(body);
+                XmlNode root = xmlD.SelectSingleNode("//xml");
+
+                string eventStr = "";
+                string eventKey = "";
+                string content = "";
+                string msgId = "";
+                string msgType = root.SelectSingleNode("MsgType").InnerText.Trim();
+
+                if (msgType.Trim().Equals("event"))
+                {
+                    eventStr = root.SelectSingleNode("Event").InnerText.Trim();
+                    eventKey = root.SelectSingleNode("EventKey").InnerText.Trim();
+                }
+                else
+                {
+                    content = root.SelectSingleNode("Content").InnerText.Trim();
+                    msgId = root.SelectSingleNode("MsgId").InnerText.Trim();
+                    msgType = root.SelectSingleNode("MsgType").InnerText.Trim();
+                }
+
+                /*
+                OAReceive msg = new OAReceive()
+                {
+                    id = 0,
+                    ToUserName = root.SelectSingleNode("ToUserName").InnerText.Trim(),
+                    FromUserName = root.SelectSingleNode("FromUserName").InnerText.Trim(),
+                    CreateTime = root.SelectSingleNode("CreateTime").InnerText.Trim(),
+                    MsgType = msgType,
+                    Event = eventStr,
+                    EventKey = eventKey,
+                    MsgId = msgId,
+                    Content = content
+
+                };
+                await _db.oAReceive.AddAsync(msg);
+                await _db.SaveChangesAsync();
+                OfficailAccountReply reply = new OfficailAccountReply(_db, _config, msg);
+                return reply.Reply().Trim();
                 */
+                return "success";
+            }
+            catch
+            {
+
+            }
+            return "success";
+        }
+
+
+
         [HttpGet]
         public  async Task<ActionResult<Code2Session>> Login(string code)
         {
