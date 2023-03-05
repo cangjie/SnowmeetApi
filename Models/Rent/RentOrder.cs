39283e820d9d017088061d1424339d639f472c2e
cangjie <cangjie@hotmail.com>
2023-03-05 23:11:56 +0800

    set status

--- Models/Rent/RentOrder.cs
+++ Models/Rent/RentOrder.cs
@@ -106,7 +106,8 @@ namespace SnowmeetApi.Models.Rent
                 double totalRental = 0;
                 for (int i = 0; details != null && i < details.Length; i++)
                 {
-                    if (details[i].real_end_date == null && details[i].start_date != null)
+                    //if (details[i].real_end_date == null && details[i].start_date != null)
+                    if (details[i].status.Trim().Equals("已领取"))
                     {
                         finish = false;
                         break;
