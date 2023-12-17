8ed0b10eafd6b33552e277f806a3085d17945da8
苍杰 <cangjie@hotmail.com>
2023-12-17 16:14:38 +0800

    change port

--- Program.cs
+++ Program.cs
@@ -21,7 +21,7 @@ namespace SnowmeetApi
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                    webBuilder.UseUrls("http://localhost:5000");
+                    webBuilder.UseUrls("http://localhost:8848");
                 });
     }
 }
