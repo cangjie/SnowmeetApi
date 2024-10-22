8f308e53c251e159b9d5230339799920586733d6
cangjie <cangjie@hotmail.com>
2024-10-22 12:25:57 +0800

    shop sale member

--- Models/Order/ShopSaleInteract.cs
+++ Models/Order/ShopSaleInteract.cs
@@ -28,6 +28,9 @@ namespace SnowmeetApi.Models.Order
                 _miniAppUser = value;
             } 
         }
+
+        [NotMapped]
+        public Member member {get; set;} = null;
     }
 }
 
