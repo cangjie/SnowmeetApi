014d39cdf3bf5c3de720a65610dbf11fdf127b94
cangjie <cangjie@hotmail.com>
2025-02-07 10:55:13 +0800

    add class

---
+++ Models/Deposit/DepositAccount.cs
@@ -0,0 +1,29 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace SnowmeetApi.Models.Deposit
+{
+    public enum DepositType
+    {
+        现金预存,服务储值
+    }
+    public class DepositAccount
+    {
+        [Key]
+        public int id { get; set; }
+        public int member_id { get; set; }
+        public string type { get; set; }
+        public DepositType sub_type { get; set; }
+        public string deposit_no { get; set; }
+        public int? order_id { get; set; }
+        public int? biz_id { get; set; }
+        public string? source { get; set; }
+        public string memo { get; set; }
+        public int income_amount { get; set; }
+        public int consume_amount { get; set; }
+        public DateTime? expire_date { get; set; }
+        public DateTime? update_date { get; set; }
+        public DateTime create_date { get; set; }
+
+    }
+}
---
+++ Models/Deposit/DepositBalance.cs
@@ -0,0 +1,21 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace SnowmeetApi.Models.Deposit
+{
+    public class DepositBalance
+    {
+        [Key]
+        public int id { get; set; }
+        public int deposit_id { get; set; }
+        public int member_id { get; set; }
+        public double amount { get; set; }
+        public int? payment_id { get; set; }
+        public int? order_id { get; set; }
+        public DateTime? extend_expire_date { get; set; }
+        public string? memo { get; set; }
+        public int? biz_id { get; set; }
+        public string? source { get; set; }
+        public DateTime create_date { get; set; }
+    }
+}
