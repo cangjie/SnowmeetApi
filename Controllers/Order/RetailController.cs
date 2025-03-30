d49f0c8e1aac8153538ff6c675e28539bfec0294
cangjie <cangjie@hotmail.com>
2025-03-30 21:09:23 +0800

    set font

--- Controllers/Order/RetailController.cs
+++ Controllers/Order/RetailController.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
 using SnowmeetApi.Data;
 using SnowmeetApi.Models;
 using SnowmeetApi.Models.Order;
@@ -24,14 +26,65 @@ namespace SnowmeetApi.Controllers
         [HttpGet]
         public async Task ExportMi7Order(DateTime startDate)
         {
+
+
+            
             List<Models.Order.Retail> rl = (List<Models.Order.Retail>)((OkObjectResult)(await ShowMi7Order(startDate)).Result).Value;
             List<Models.Mi7ExportedSaleDetail> details = await _db.mi7ExportedSaleDetail.ToListAsync();
+            int maxPaymentNum = 0;
+            int maxRefundNum = 0;
             for(int i = 0; i < rl.Count; i++)
             {
                 Retail r = rl[i];
                 r.details = details.Where(d => d.单据编号.Trim().Equals(r.mi7OrderId.Trim())).ToList();
+                maxPaymentNum = Math.Max(r.payments.Count, maxPaymentNum);
+                maxRefundNum = Math.Max(r.refunds.Count, maxRefundNum);
             }
-    
+            List<string> head = [
+                "序号", "七色米订单号",	"店铺", "业务类型", "业务日期", "业务时间", "开单日期", "开单时间", "发货日期",
+                "开单明细数", "零售总价", "成交总价", "支付笔数", "支付金额", "退款笔数", "退款金额", "支付方式", "商品类别"];
+            string[] headPayment = ["商品名称支付方式", "收款单号", "收款日期", "收款时间"];
+            string[] headRefund = ["退款单号", "退款金额", "退款日期", "退款时间"];
+            for(int i = 0; i < maxPaymentNum; i++)
+            {
+                for(int j = 0; j < headPayment.Length; j++)
+                {
+                    head.Add(headPayment[j] + (i+1).ToString());
+                }
+            }
+            for(int i = 0; i < maxRefundNum; i++)
+            {
+                for(int j = 0; j < headRefund.Length; j++)
+                {
+                    head.Add(headRefund[j] + (i+1).ToString());
+                }
+            }
+            string nullStr = "【-】";
+            XSSFWorkbook workbook = new XSSFWorkbook();
+            ISheet sheet = workbook.CreateSheet("Sheet1");
+            IRow headRow = sheet.CreateRow(0);
+            IFont headFont = workbook.CreateFont();
+            headFont.Color = NPOI.HSSF.Util.HSSFColor.White.Index;
+            headFont.IsBold = true;
+            ICellStyle headStyle = workbook.CreateCellStyle();
+            headStyle.Alignment = HorizontalAlignment.Center;
+            headStyle.FillForegroundColor = NPOI.HSSF.Util.HSSFColor.Black.Index;
+            headStyle.FillPattern = FillPattern.SolidForeground;
+            headStyle.SetFont(headFont);
+            //workbook.get
+            //headStyle.FillPattern = FillPatternType
+            for(int i = 0; i < head.Count; i++)
+            {
+                ICell headCell = headRow.CreateCell(i);
+                headCell.SetCellValue(head[i].Trim());
+                headCell.SetCellType(CellType.String);
+                headCell.CellStyle = headStyle;
+               
+            }
+            using(var file = System.IO.File.Create("mi7.xlsx"))
+            {
+                workbook.Write(file);
+            }
         }
         [HttpGet]
         public async Task<ActionResult<List<Models.Order.Retail>>> ShowMi7Order(DateTime startDate)
