87cff7425fc4a045f67b6f8a4d182034a96d09cb
cangjie <cangjie@hotmail.com>
2025-04-08 20:49:36 +0800

    biz date

--- Controllers/Order/OrderOnlinesController.cs
+++ Controllers/Order/OrderOnlinesController.cs
@@ -427,7 +427,7 @@ namespace SnowmeetApi.Controllers
                     )))
                 
                 .Where(o => (  
-                o.create_date >= startDate && o.create_date <= endDate && (shop.Trim().Equals("") ? true : (o.shop.Trim().Equals(shop.Trim())))
+                o.biz_date >= startDate && o.biz_date <= endDate && (shop.Trim().Equals("") ? true : (o.shop.Trim().Equals(shop.Trim())))
                  && (!onlyMine || (onlyMine && o.staff_open_id.Trim().Equals(user.miniAppOpenId.Trim())) ) 
                  && (cell.Length < 4 || o.cell_number.EndsWith(cell.Trim()) || o.open_id.Trim().Equals(openId) ) 
                  && (orderId.Equals("0") || o.id == int.Parse(orderId))
