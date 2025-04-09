184b13864b49cec80f59e0312470cb6c21d3b23d
cangjie <cangjie@hotmail.com>
2025-04-09 12:31:33 +0800

    set biz date

--- Controllers/Order/OrderOnlinesController.cs
+++ Controllers/Order/OrderOnlinesController.cs
@@ -1224,7 +1224,33 @@ namespace SnowmeetApi.Controllers
             _context.OrderOnlines.Entry(order).State = EntityState.Modified;
             await _context.SaveChangesAsync();
         }
-
+        [HttpGet("{orderId}")]
+        public async Task<ActionResult<OrderOnline>> UpdateOrderBizDate(int orderId,
+            DateTime bizDate, string sessionKey, string sessionType = "wechat_mini_openid")
+        {
+            UnicUser user = await  UnicUser.GetUnicUserAsync(sessionKey, _context);
+            if (!user.isAdmin)
+            {
+                return BadRequest();
+            }
+            OrderOnline order = await _context.OrderOnlines.FindAsync(orderId);
+            StaffModLog log = new StaffModLog()
+            {
+                id = 0,
+                key_id = order.id.ToString(),
+                scene = "修改零售业务时间",
+                table_name = "order_online",
+                field_name = "biz_date",
+                prev_value = order.biz_date.ToString(),
+                current_value = bizDate.ToString(),
+                staff_member_id = user.member.id
+            };
+            await _context.staffModLog.AddAsync(log);
+            order.biz_date = bizDate;
+            _context.OrderOnlines.Entry(order).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return Ok(order);
+        }
 
         [NonAction]
         private bool OrderOnlineExists(int id)
