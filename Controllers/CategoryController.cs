163ec913c375998ea9c761000e56cd0de91594aa
cangjie <cangjie@hotmail.com>
2025-07-25 15:12:41 +0800

    order by

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -413,7 +413,7 @@ namespace SnowmeetApi.Controllers
             return await _db.product.Where(p => p.category_id == categoryId && p.valid == 1)
                 .Include(p => p.images.OrderBy(i => i.sort))
                 .Include(t => t.properties.Where(p => p.valid == 1))
-                    .ThenInclude(p => p.categoryProperty).ThenInclude(c => c.options).OrderBy(t => t.sort)
+                    .ThenInclude(p => p.categoryProperty).ThenInclude(c => c.options).OrderByDescending(t => t.sort)
                 .AsNoTracking().ToListAsync();
 
         }
