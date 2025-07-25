f7b3185cbe303929d71a4b5d77ac9112cb7baaaa
苍杰 <cangjie@hotmail.com>
2025-07-25 11:27:01 +0800

    no entrain

--- Models/Product.cs
+++ Models/Product.cs
@@ -22,6 +22,7 @@ namespace SnowmeetApi.Models
         public int hidden { get; set; } = 1;
         public int valid { get; set; } = 0;
         public int on_shelves { get; set; } = 0;
+        public int no_entrain { get; set; } = 0;
         public int sort { get; set; } = 100;
         public int? resort_id { get; set; } = null;
         public int? stock_num { get; set; } = null;
