3ddf89afbf334618b97ebd8d6b20fb9a7bd4f9ba
cangjie <cangjie@hotmail.com>
2025-08-15 10:43:45 +0800

    status

--- Models/Order/Order.cs
+++ Models/Order/Order.cs
@@ -704,7 +704,7 @@ namespace SnowmeetApi.Models
                             return "待生成";
                         }
                     }
-                    if (pay_flow_status != null && (pay_flow_status.Trim().Equals("待支付") || pay_flow_status.Trim().Equals("支付中")))
+                    if (pay_flow_status != null && (pay_flow_status.Trim().Equals("已生成") || pay_flow_status.Trim().Equals("待支付") || pay_flow_status.Trim().Equals("支付中")))
                     {
                         if ((dealed == 0 && totalCharge > 0) || (creditAmount > 0 && paidAmount == 0))
                         {
