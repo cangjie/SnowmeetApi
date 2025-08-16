2168ffb3835117c6913af9f1b2baed9ac1965de4
cangjie <cangjie@hotmail.com>
2025-08-16 15:52:05 +0800

    order's payments

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -1145,9 +1145,9 @@ namespace SnowmeetApi.Controllers
             bool canceled = true;
             AliController _aliHelper = new AliController(_db, _config, _http);
             TenpayController _weHelper = new TenpayController(_db, _config, _http);
-            List<OrderPayment> payments = await _db.orderPayment
-                .Where(p => p.valid == 1 && (p.pay_method.Trim().Equals("微信支付") || p.pay_method.Trim().Equals("支付宝"))
-                && p.status.Trim().Equals(OrderPayment.PaymentStatus.支付成功.ToString()) && p.order_id == order.id).ToListAsync();
+            List<OrderPayment> payments = order.availablePayments
+                .Where(p => (p.pay_method.Trim().Equals("微信支付") || p.pay_method.Trim().Equals("支付宝"))
+                && !p.status.Trim().Equals(OrderPayment.PaymentStatus.支付成功.ToString())).ToList();
             for (int i = 0; i < payments.Count; i++)
             {
                 OrderPayment payment = payments[i];
