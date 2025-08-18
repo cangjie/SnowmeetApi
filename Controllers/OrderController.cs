125497e5ea783b1663c362fb51af5426969a9a63
cangjie <cangjie@hotmail.com>
2025-08-18 15:33:04 +0800

    refresh order

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -1183,33 +1183,31 @@ namespace SnowmeetApi.Controllers
         {
             DateTime startTime = DateTime.Now;
             Models.Order order = await _db.order.FindAsync(orderId);
-            if (order.dealed == 1)
-            {
-                order.queryed = 1;
-                order.update_date = DateTime.Now;
-                _db.order.Entry(order).State = EntityState.Modified;
-                await _db.SaveChangesAsync();
-                return order;
-            }
+            
             OrderPayment payment = await _db.orderPayment.Where(p => p.order_id == orderId && p.valid == 1 && p.queryed == 0
                  && p.status.Trim().Equals(OrderPayment.PaymentStatus.支付成功.ToString())
                  && p.paid_date > DateTime.Now.AddHours(-4))
                  .OrderByDescending(p => p.id).FirstOrDefaultAsync();
-            
-            for (; (payment == null && (DateTime.Now - startTime).Seconds <= 3600);)
+
+            for (; (payment == null && order.dealed == 0 && (DateTime.Now - startTime).Seconds <= 3600);)
             {
                 Thread.Sleep(1000);
                 payment = await _db.orderPayment.Where(p => p.order_id == orderId && p.valid == 1 && p.queryed == 0
                  && p.status.Trim().Equals(OrderPayment.PaymentStatus.支付成功.ToString())
                  && p.paid_date > DateTime.Now.AddHours(-4))
                  .OrderByDescending(p => p.id).FirstOrDefaultAsync();
+                 order = await _db.order.FindAsync(orderId);
             }
             if (payment != null)
             {
                 payment.queryed = 1;
                 _db.orderPayment.Entry(payment).State = EntityState.Modified;
                 await _db.SaveChangesAsync();
-            } 
+            }
+            order.queryed = 1;
+            order.update_date = DateTime.Now;
+            _db.order.Entry(order).State = EntityState.Modified;
+            await _db.SaveChangesAsync();
             return order;
         }
         [HttpGet("{orderId}")]
