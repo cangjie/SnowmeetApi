cc072423ab107a5dad95514265a77f628c596be5
cangjie <cangjie@hotmail.com>
2025-09-09 14:30:33 +0800

    shop business

--- Models/Shop.cs
+++ Models/Shop.cs
@@ -10,12 +10,16 @@ namespace SnowmeetApi.Models
         [Key]
         public int id { get; set; }
         public string name { get; set; }
-        public string code {get; set;}
+        public string code { get; set; }
         public int sort { get; set; }
         public double lat_from { get; set; }
         public double lat_to { get; set; }
         public double long_from { get; set; }
         public double long_to { get; set; }
+        public int sale { get; set; }
+        public int care { get; set; }
+        public int rent { get; set; }
+        public int restuarant { get; set; }
     }
 }
 
