0bcd25c4f6a908da318764a205de2713631c2bad
cangjie <cangjie@hotmail.com>
2025-09-13 08:21:38 +0800

    new start

--- Models/School/Staff.cs
+++ Models/School/Staff.cs
@@ -14,21 +14,15 @@ namespace SnowmeetApi.Models.School
 	    public string school_name {get; set;}
 	    public string? sub_school_name {get; set;}
 	    public string? team {get; set;}
-
         public string? temp_filled_name {get; set;} 
         public string? temp_filled_cell {get; set;}
         public string? temp_filled_gender {get; set;}
-
         public string avatar {get; set;}
 	    public string role {get; set;}
 	    public DateTime create_date {get; set;} = DateTime.Now;
-
         public string? grade {get; set;}
-
         public string? ski_level {get; set;}
-
         public string? board_level {get; set;}
-
         [NotMapped]
         public Member member {get; set;}
     }
