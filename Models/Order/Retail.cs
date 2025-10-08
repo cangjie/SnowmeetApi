5b50da0ca12fd53f1d8776af83f2a4b9cf32898d
cangjie <cangjie@hotmail.com>
2025-10-08 15:20:21 +0800

    giveup score

--- Models/Order/Retail.cs
+++ Models/Order/Retail.cs
@@ -56,7 +56,8 @@ namespace SnowmeetApi.Models
         public double deal_price {get; set;} = 0;
         public string? order_type {get; set;} = null;
         public int valid {get; set;} = 1;
-        public DateTime? update_date {get; set;} = null;
+        public int giveup_score { get; set; } = 0;
+        public DateTime? update_date { get; set; } = null;
         public DateTime create_date {get; set;} = DateTime.Now;
         [NotMapped]
         public string textColor { get; set; } = "";
