d4a0355f24e1efb2953debff08b3235f35ebf727
cangjie <cangjie@hotmail.com>
2025-10-25 16:54:33 +0800

    report

--- wwwroot/background/js/menu.js
+++ wwwroot/background/js/menu.js
@@ -1,9 +1,10 @@
-﻿var menuItems = [{ title: '销售业务报表', link: '../sale/sale_report_temp.html' },
-    { title: '养护业务报表', link: '../maintain/task_report_temp.html' },
-    { title: '租赁业务报表', link: '../rent/rent_report_temp.html' },
-    { title: '租赁订单报表(交易表)', link: '../rent/rent_order_list.html' },
-    { title: '微信支付订单(交易表)', link: '../wepay/wepay_list.html' },
-    { title: '大好河山对账单', link: '../skipass/dhhs_list.html' }];
+﻿var menuItems = [{ title: '销售业务临时报表', link: '../sale/sale_report_temp.html' },
+    { title: '养护业务临时报表', link: '../maintain/maintain_report_temp.html' },
+    { title: '租赁业务临时报表', link: '../rent/rent_report_temp.html' }
+    //{ title: '租赁订单报表(交易表)', link: '../rent/rent_order_list.html' },
+    //{ title: '微信支付订单(交易表)', link: '../wepay/wepay_list.html' },
+    //{ title: '大好河山对账单', link: '../skipass/dhhs_list.html' }
+    ];
 var width = 100 / menuItems.length;
 var menu = document.getElementById('menu');
 for (var i = 0; i < menuItems.length; i++) {
