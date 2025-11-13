d95229a0d93dc27d063b44557cdd802763122789
cangjie <cangjie@hotmail.com>
2025-11-13 11:27:28 +0800

    alert

--- wwwroot/js/get_sessionkey.js
+++ wwwroot/js/get_sessionkey.js
@@ -1,11 +1,10 @@
 ﻿var sessionKey = getCookie('sessionKey');
 var titleLevel = getCookie('title_level');
 alert(titleLevel);
-if (titleLevel == 50){
-    window.location.href = '/background/rent/wl_list.html';
-}
+alert(sessionKey);
 //sessionKey = '%2F4mpN1%2FtWiXk0N3%2BR55KVw%3D%3D';
 var orderDetails = [];
 if (sessionKey == '') {
+
     window.location.href = '/background/index.html';
 }
