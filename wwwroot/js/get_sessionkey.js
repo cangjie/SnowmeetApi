21bb263f375c61c299a5acdb4d7ceb0b3c718902
cangjie <cangjie@hotmail.com>
2025-11-13 12:08:32 +0800

    render

--- wwwroot/js/get_sessionkey.js
+++ wwwroot/js/get_sessionkey.js
@@ -10,6 +10,5 @@ if (titleLevel == 50){
 //sessionKey = '%2F4mpN1%2FtWiXk0N3%2BR55KVw%3D%3D';
 var orderDetails = [];
 if (sessionKey == '') {
-
     window.location.href = '/background/index.html';
 }
