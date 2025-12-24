f74ca8c88cac12cff7276a8190713363e8608a42
cangjie <cangjie@hotmail.com>
2025-12-24 22:42:38 +0800

    by scene

--- Controllers/PrinterController.cs
+++ Controllers/PrinterController.cs
@@ -27,7 +27,6 @@ namespace SnowmeetApi.Controllers
                 .AsNoTracking().ToListAsync();
             return Ok(l);
         }
-        /*
         [HttpGet]
         public async Task<ActionResult<ApiResult<List<Printer>>>> GetPrinterByScene(string shop)
         {
@@ -42,7 +41,6 @@ namespace SnowmeetApi.Controllers
                 data = l
             });
         }
-        */
         [HttpGet]
         public async Task<ActionResult<ApiResult<List<PrintTask>>>> RefreshPrintTask(string shop, DateTime startDate)
         {
