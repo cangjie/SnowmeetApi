64599b2c34554fd7bad4038cd95d914da90f9668
cangjie <cangjie@hotmail.com>
2025-12-29 15:23:48 +0800

    rentals

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -52,7 +52,7 @@ namespace SnowmeetApi.Controllers
                     .ThenInclude(i => i.logs.OrderByDescending(o => o.id))
                         .ThenInclude(l => l.staff)
                 .Include(r => r.guaranties.Where(g => g.valid == 1 && g.biz_type.Trim().Equals("租赁"))).ThenInclude(g => g.guarantyPayments).ThenInclude(g => g.payment)
-                .AsNoTracking().ToListAsync();
+                .Where(r => r.valid == 1).AsNoTracking().ToListAsync();
             order.skipasses = await _db.order.Entry(order).Collection(o => o.skipasses).Query()
                 .Where(s => s.valid == 1).Include(s => s.skiPassProduct).ThenInclude(p => p.dailyPrice).AsNoTracking().ToListAsync();
             order.discounts = await _db.order.Entry(order).Collection(o => o.discounts).Query().Where(d => d.valid == 1).ToListAsync();
