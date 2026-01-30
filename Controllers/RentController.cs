a564c4fcd5651d2a8606c51df3b71726e6f142aa
cangjie <cangjie@hotmail.com>
2026-01-30 11:20:51 +0800

    non

--- Controllers/RentController.cs
+++ Controllers/RentController.cs
@@ -4221,7 +4221,7 @@ namespace SnowmeetApi.Controllers
                     item.category = await _db.rentCategory.Where(c => c.id == item.category_id).AsNoTracking().FirstOrDefaultAsync();
                 }
             }
-            order = await AddInterCom(order);
+            //order = await AddInterCom(order);
             return Ok(new ApiResult<Models.Order?>()
             {
                 code = 0,
