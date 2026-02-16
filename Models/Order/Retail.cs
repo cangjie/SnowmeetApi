2c5972e6258f3be3a89ff5b029a27ffafbf42e41
苍杰 <cangjie@hotmail.com>
2026-02-16 21:19:39 +0800

    default true

--- Models/Order/Retail.cs
+++ Models/Order/Retail.cs
@@ -78,7 +78,7 @@ namespace SnowmeetApi.Models
         public int id { get; set; }
         public int order_id { get; set; }
         public int image_id { get; set; }
-        public bool valid {get; set;}
+        public bool valid {get; set;} = true;
         [ForeignKey("order_id")]
         public Models.Order? order { get; set; } = null;
         [ForeignKey("image_id")]
