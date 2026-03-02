dfc721744c04a4cb259b83b99014412ecf85fdc2
cangjie <cangjie@hotmail.com>
2026-03-02 16:44:39 +0800

    short name

--- Models/Rent/RentCategory.cs
+++ Models/Rent/RentCategory.cs
@@ -11,6 +11,7 @@ namespace SnowmeetApi.Models
         public int id {get; set;}
         public string code { get; set; }
         public string name { get; set; }
+        public string? short_name { get; set; } = null;
         public double deposit {get; set;}
         public int valid { get; set; } = 1;
         public int? staff_id { get; set; } = null;
