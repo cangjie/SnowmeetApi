4d6e94887a60b386a581cf560b26099448007b6b
苍杰 <cangjie@hotmail.com>
2026-03-09 19:57:44 +0800

    father

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -141,6 +141,11 @@ namespace SnowmeetApi.Controllers
             switch (type)
             {
                 case "租赁":
+                    RentCategory father = null;
+                    if (rentCategoryId != null)
+                    {
+                        father = await _db.rentCategory.Where(r => r.id == rentCategoryId).AsNoTracking().FirstOrDefaultAsync();
+                    }
                     orderList = await _db.order.Where(o => (o.biz_date.Date >= ((DateTime)startDate).Date && o.biz_date.Date <= ((DateTime)endDate).Date)
                         && (memberId == null || o.member_id == memberId) && (staffId == null || o.staff_id == staffId)
                         && (payOption == null || o.pay_option.Trim().Equals(payOption.Trim()))
@@ -149,14 +154,15 @@ namespace SnowmeetApi.Controllers
                         && (closeStartDate == null || (o.close_date != null && ((DateTime)o.close_date).Date >= ((DateTime)closeStartDate).Date))
                         && (closeEndDate == null || (o.close_date != null && ((DateTime)o.close_date).Date <= ((DateTime)closeEndDate).Date))
                         && (rentCategoryId == null || (
-                            o.rentals.Any(r => r.order_id == o.id && r.valid == 1 && r.rentItems.Any(i => i.valid == 1 && i.rental_id == r.id && i.category.father.id == rentCategoryId))
+                            o.rentals.Any(r => r.order_id == o.id && r.valid == 1 && r.rentItems.Any(i => i.valid == 1 && i.rental_id == r.id 
+                            && father != null && i.category.code.IndexOf(father.code) == 0)
 
                         ) )
                         && (rentItemName == null || (
                             o.rentals.Any(r => r.valid==1 && r.order_id == o.id && r.rentItems.Any(i => i.valid == 1 && i.rental_id == r.id && (i.name.IndexOf(rentItemName) >= 0 || i.code.IndexOf(rentItemName) >= 0 ) )  )
                         ))
                         
-                        )
+                        ))
                     .Include(o => o.rentals.Where(r => r.valid == 1 && (r.appending == null || (r.appending == false && r.append_commit_time != null)))).ThenInclude(r => r.details.Where(d => d.valid == 1)).ThenInclude(d => d.discounts.Where(d => d.valid == 1 && d.sub_biz_type == "日租金"))
                     .Include(o => o.rentals.Where(r => r.valid == 1 && (r.appending == null || (r.appending == false && r.append_commit_time != null)))).ThenInclude(r => r.discounts.Where(d => d.valid == 1 && d.biz_type == "租赁"))
                     .Include(o => o.rentals.Where(r => r.valid == 1 && (r.appending == null || (r.appending == false && r.append_commit_time != null)))).ThenInclude(r => r.rentItems.Where(r => r.valid == 1))
