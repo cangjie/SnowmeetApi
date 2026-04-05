566dea1760ce568148c0f5e12536bfa7b5b1913d
苍杰 <cangjie@hotmail.com>
2026-04-05 16:49:51 +0800

    biz date

--- Controllers/RentController.cs
+++ Controllers/RentController.cs
@@ -5560,7 +5560,7 @@ namespace SnowmeetApi.Controllers
             OrderController _orderHelper = new OrderController(_db, _config, _httpContextAccessor);
             List<Models.Order> orders = (await _orderHelper.GetCommonOrders(null, shop, null, null, "租赁",
                 null, null, null, false, null, false, false, null, null, startDate, endDate))
-                .OrderByDescending(o => o.code).ToList();
+                .OrderByDescending(o => o.biz_date).ToList();
             List<Models.Order> confirmedOrders = new List<Models.Order>();
             for (int i = 0; i < orders.Count; i++)
             {
